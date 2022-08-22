f7e01596b83be33d8a2756c05d6448448ab9caad
shabeebas <shabeebasahad2020@gmail.com>
2022-08-22 10:56:55 +0530

    horoku

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m3)!b)9eb48oqpamf)l%xeqhe4y29v988&j^w!7$qj3j(5=78d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'firstproject.urls'
